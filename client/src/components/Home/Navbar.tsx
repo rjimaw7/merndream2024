09f6355ef3796ce707e2e9cbdbeefc0632dd2c99
rjimaw7 <rjimaw7777@gmail.com>
2024-01-24 16:23:32 +0800

    replaced navbar icon

--- client/src/components/Home/Navbar.tsx
+++ client/src/components/Home/Navbar.tsx
@@ -1,12 +1,15 @@
-import ReactSVG from "@/assets/react.svg";
 import { Input } from "../ui/input";
 import CustomSwitch from "./CustomSwitch";
+import { CloudSun, MoonStar } from "lucide-react";
+import { useTheme } from "../theme-provider";
 
 const Navbar = () => {
+  const { theme } = useTheme();
+
   return (
     <header className="flex justify-between items-center py-3 px-12">
       <div>
-        <img src={ReactSVG} alt="headerlogo" />
+        {theme === "light" ? <CloudSun size={32} /> : <MoonStar size={32} />}
       </div>
       <div className="flex items-center gap-2">
         <Input type="search" placeholder="Search Dreams" />
